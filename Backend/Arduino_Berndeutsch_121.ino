c9765019da21ddaa0c55b1573fd89d9114450a2c
U109281 <U109281@mobi.ch>
2024-01-17 16:09:22 +0100

    Initial release

---
+++ Backend/Arduino_Berndeutsch_121.ino
@@ -0,0 +1,469 @@
+/////////////////////////////////////////////
+//
+// LOLIN (WEMOS) D1 mini Lite (ESP8266) Wordclock Program
+// Based on sripts and snippets from:
+// Rui Santos http://randomnerdtutorials.com
+// neotrace https://www.instructables.com/id/WORK-IN-PROGRESS-Ribba-Word-Clock-With-Wemos-D1-Mi/
+// and others
+//
+// Kurt Meister, 2018-12-24 | Edit: 2023-04-29 
+// Thanks to Manuel Meister for refactoring and adding automated summertime conversion.
+//
+/////////////////////////////////////////////
+
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>        // v2.4.2
+#include <WiFiManager.h>        // v2.0.3-alpha
+#include <WiFiUdp.h>
+#include <TimeLib.h>            // v1.6.1
+#include <Timezone.h>           // v1.2.4
+#include <Adafruit_NeoPixel.h>  // v1.10.4
+
+char version[] = "V3";
+
+int wordClockMinute = 0;
+int wordClockHour = 0;
+int lastMinuteWordClock = 61;
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+static const char ntpServerName[] = "0.ch.pool.ntp.org";
+
+const int timeZone = 0;     // Central European Time
+
+// function definitions
+time_t getNtpTime();
+
+void formatDigits(int digits);
+
+void serialTime();
+
+void sendNTPpacket(IPAddress &address);
+
+//Central European Time (Frankfurt, Paris)
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
+TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 60};       //Central European Standard Time
+Timezone CE(CEST, CET);
+time_t timeWithDST;
+
+WiFiUDP ntpUDP;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+
+static uint32_t Black = Adafruit_NeoPixel::Color(0, 0, 0);
+static uint32_t White = Adafruit_NeoPixel::Color(49, 52, 34);
+static uint32_t Green = Adafruit_NeoPixel::Color(10, 90, 0);
+static uint32_t Red = Adafruit_NeoPixel::Color(90, 0, 0);
+static uint32_t Orange = Adafruit_NeoPixel::Color(70, 30, 0);
+static uint32_t Yellow = Adafruit_NeoPixel::Color(50, 50, 0);
+static uint32_t Blue = Adafruit_NeoPixel::Color(0, 20, 85);
+static uint32_t DarkBlue = Adafruit_NeoPixel::Color(0, 10, 40);
+static uint32_t DarkestBlue = Adafruit_NeoPixel::Color(0, 8, 20);
+
+/*
+| Ä | S | U | I | S | C | H | J | F | Ü | F |
+| V | I | E | R | T | U | X | B | Z | Ä | D |
+| Z | W | Ä | N | Z | G | F | R | V | O | R |
+| R | A | B | I | H | A | U | B | I | N | S |
+| E | I | S | C | Z | W | Ö | I | D | R | Ü |
+| V | V | I | E | R | I | F | Ü | F | I | E |
+| S | Ä | C | H | S | I | S | I | B | N | I |
+| D | A | C | H | T | I | N | Ü | N | I | K |
+| N | Z | Ä | N | I | E | U | F | I | O | G |
+| S | G | Y | Z | W | Ö | U | F | I | W | N |
+| W | X | C | * | * | C | * | * | H | L | G |
+ */
+
+static int WordEs[] = {0, 1, -1};
+static int WordIst[] = {3, 4, 5, 6, -1};
+static int WordHalb[] = {39, 38, 37, 36, 35, -1};
+
+static int WordFix[] = {77, 98, 99, -1};
+static int WordWifi[] = {120, -1};
+static int WordNach[] = {42, 41, -1};
+static int WordVor[] = {30, 31, 32, -1};
+
+static int SymbolWifi[] = {120, -1};
+
+static int *WordURL[] = {WordFix, WordWifi};
+
+// Stunde
+static int WordStundeEins[] = {44, 45, 46, -1};                     // EIS
+static int WordStundeZwei[] = {48, 49, 50, 51, -1};                 // ZWÖI
+static int WordStundeDrei[] = {52, 53, 54, -1};                     // DRÜ
+static int WordStundeVier[] = {64, 63, 62, 61, 60, -1};             // VIERI
+static int WordStundeFuenf[] = {59, 58, 57, 56, -1};                // FÜFI
+static int WordStundeSechs[] = {66, 67, 68, 69, 70, 71, -1};        // SÄCHSI
+static int WordStundeSieben[] = {72, 73, 74, 75, 76, -1};           // SIBNI
+static int WordStundeAcht[] = {87, 86, 85, 84, 83, -1};             // ACHTI
+static int WordStundeNeun[] = {82, 81, 80, 79, -1};                 // NÜNI
+static int WordStundeZehn[] = {89, 90, 91, 92, -1};                 // ZÄNI
+static int WordStundeElf[] = {93, 94, 95, 96, -1};                  // EUFI
+static int WordStundeZwoelf[] = {106, 105, 104, 103, 102, 101, -1}; // ZWÖUFI
+
+static int *WordStunden[] = {WordStundeZwoelf, WordStundeEins, WordStundeZwei, WordStundeDrei, WordStundeVier,
+                             WordStundeFuenf, WordStundeSechs, WordStundeSieben, WordStundeAcht, WordStundeNeun,
+                             WordStundeZehn, WordStundeElf, WordStundeZwoelf
+};
+
+// Minute
+static int WordMinFuenf[] = {8, 9, 10, -1};                   // FÜF
+static int WordMinZehn[] = {13, 12, -1};                      // ZÄ
+static int WordMinViertel[] = {21, 20, 19, 18, 17, 16, -1};   // VIERTU
+static int WordMinZwanzig[] = {22, 23, 24, 25, 26, 27, -1};   // ZWÄNZG
+static int WordMinTicks[] = {113, 114, 116, 117, -1};
+
+static int *WordMinuten[] = {WordMinFuenf, WordMinZehn, WordMinViertel, WordMinZwanzig, WordMinFuenf};
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(121, D7, NEO_GRB + NEO_KHZ800);
+uint32_t foregroundColor = White;
+uint32_t backgroundColor = Black;
+
+
+// Don't light up WiFi, only on requests
+uint32_t wifiColor = Black;
+
+// How long this state should be displayed
+int wifiWait = 0;
+
+/**
+ * Runs through all pixels
+ * @param color
+ */
+void chase(uint32_t color) {
+  for (uint16_t i = 0; i < pixels.numPixels() + 4; i++) {
+    pixels.setPixelColor(i, color); // Draw new pixel
+    pixels.setPixelColor(i - 4, backgroundColor); // Erase pixel a few steps back
+    pixels.show();
+    delay(25);
+  }
+}
+
+/**
+ * Shows logo or/and fix wifi
+ * @param color
+ */
+void showIntro(uint32_t color) {
+  for (int j = 0; j < 2; j++) {
+    if (WordURL[j] == WordFix) {
+      delay(450);
+    }
+    for (int i = 0; i < pixels.numPixels(); i++) {
+      if (WordURL[j][i] == -1) {
+        break;
+      } else {
+        pixels.setPixelColor(WordURL[j][i], White); // Erase pixel a few steps back
+      }
+      pixels.show();
+      delay(25);
+    }
+    pixels.show();
+    delay(250);
+  }
+}
+
+/**
+ * Sets all pixels to the background
+ */
+void blank() {
+  for (int x = 0; x < pixels.numPixels(); ++x) {
+    pixels.setPixelColor(x, backgroundColor);
+  }
+}
+
+/**
+ * Sets all pixels to the background and displays it
+ */
+void wipe() {
+  blank();
+  pixels.show();
+}
+
+/**
+ * Sets array of pixels to a specific color
+ * @param Word array of the id of the pixel
+ * @param Color
+ */
+void lightup(int *Word, uint32_t Color) {
+  for (int x = 0; x < pixels.numPixels() + 1; x++) {
+    if (Word[x] == -1) {
+      Serial.print(" ");
+      break;
+    } else {
+      pixels.setPixelColor(Word[x], Color);
+    }
+  }
+}
+
+void showWifiSuccess() {
+  lightup(WordWifi, Blue);
+  lightup(SymbolWifi, Green);
+  pixels.show();
+}
+
+void connectWLAN() {
+  lightup(WordFix, White);
+  lightup(WordWifi, Blue);
+  pixels.show();
+}
+
+/**
+ * Sets the pixels for the hour
+ */
+void showHour() {
+  if (wordClockMinute < 25) {
+    // show this hour if we are before 25 minutes past
+    lightup(WordStunden[wordClockHour % 12], foregroundColor);
+  } else {
+    // show next hour
+    lightup(WordStunden[(wordClockHour % 12) + 1], foregroundColor);
+  }
+}
+
+/**
+ * Sets pixels for the current minutes values
+ */
+void showMinute() {
+  if (wordClockMinute != 0) {
+    if (wordClockMinute >= 5 && wordClockMinute < 30) {
+      // sets pixels with minutes array
+      lightup(WordMinuten[(wordClockMinute / 5) - 1], foregroundColor);
+    } else if (wordClockMinute >= 35) {
+      // sets pixels with same array, but in reverse
+      lightup(WordMinuten[5 - ((wordClockMinute - 30) / 5)], foregroundColor);
+    }
+    if ((wordClockMinute >= 5 && wordClockMinute < 25) || (wordClockMinute < 40 && wordClockMinute >= 35)) {
+      lightup(WordNach, foregroundColor);
+    }
+    if (wordClockMinute >= 40 || (wordClockMinute >= 25 && wordClockMinute < 30)) {
+      lightup(WordVor, foregroundColor);
+    }
+    if (wordClockMinute >= 25 && wordClockMinute < 40) {
+      lightup(WordHalb, foregroundColor);
+    }
+    // Checks if the minute ticks should be displayed
+    int differenceToLast5Min = wordClockMinute % 5;
+    if (differenceToLast5Min != 0) {
+      for (int i = 0; i < differenceToLast5Min; i++) {
+        pixels.setPixelColor(WordMinTicks[i], foregroundColor);
+      }
+    }
+  }
+}
+
+/**
+ * Displays the status of the wifi in the loop
+ */
+void displayWifiStatus() {
+  if (wifiWait > 0) {
+    wifiWait--;
+  } else if (wifiWait == 0) {
+    wifiColor = Black;
+    wifiWait = -1;
+    lightup(SymbolWifi, wifiColor);
+    pixels.show();
+  }
+}
+
+void displayTime() {
+  blank();
+
+  // light up "it's" it stays on
+  lightup(WordEs, foregroundColor);
+  lightup(WordIst, foregroundColor);
+
+  showMinute();
+  showHour();
+
+  displayWifiStatus();
+
+  pixels.show();
+}
+
+void setupDisplay() {
+  pixels.begin();
+  wipe();
+}
+
+/**
+ * Sets the status of the wifi
+ * @param Color
+ * @param duration
+ */
+void setWifiStatus(uint32_t Color, int duration) {
+  wifiWait = duration;
+  wifiColor = Color;
+  lightup(SymbolWifi, wifiColor);
+  pixels.show();
+}
+
+/**
+ * Print time via serial output
+ */
+void serialTime() {
+  // digital clock display of the time
+  Serial.println("");
+  formatDigits(wordClockHour);
+  Serial.print(":");
+  formatDigits(wordClockMinute);
+  Serial.print(" - ");
+}
+
+/**
+ * utility for digital clock display: prints preceding colon and leading 0
+ * @param digits
+ */
+void formatDigits(int digits) {
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+/**
+ * Updates the global time values with the time from the NTPClient
+ */
+void getLocalTime() {
+  TimeChangeRule *tcr;
+  time_t utc;
+  utc = now();
+  timeWithDST = CE.toLocal(utc, &tcr);
+
+  // global time values
+  wordClockMinute = minute(timeWithDST);
+  wordClockHour = hour(timeWithDST);
+}
+
+time_t getNtpTime() {
+  setWifiStatus(Blue, 250);
+  IPAddress ntpServerIP; // NTP server's ip address
+  while (ntpUDP.parsePacket() > 0); // discard any previously received packets
+  Serial.println("");
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 2500) {
+    int size = ntpUDP.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      ntpUDP.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 = (unsigned long) packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long) packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long) packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long) packetBuffer[43];
+      setSyncInterval(120);
+      Serial.print("Received: ");
+      Serial.println(secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR);
+      setWifiStatus(Green, 250);
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  setWifiStatus(Red, 500000000);
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time (Timelib just queries again)
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 0x49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 0x49;
+  packetBuffer[15] = 0x52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  ntpUDP.beginPacket(address, 123); //NTP requests are to port 123
+  ntpUDP.write(packetBuffer, NTP_PACKET_SIZE);
+  ntpUDP.endPacket();
+}
+
+/**
+ * Initializes timeClient so it queries the NTP server
+ * also makes first update to sync the time
+ */
+void setupTime() {
+  Serial.println("Starting UDP");
+  ntpUDP.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(ntpUDP.localPort());
+  Serial.println("waiting for sync");
+  setSyncProvider(getNtpTime);
+  setSyncInterval(10);
+}
+
+/**
+ * Sets up wifi
+ */
+void setupWifi() {
+  wifi_station_set_hostname("wordclock");
+
+  // WiFiManager
+  // Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  // Uncomment and run it once, if you want to erase all the stored information
+  //wifiManager.resetSettings();
+
+  // set custom ip for portal
+  //wifiManager.setAPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  // Displays Wifi Connect screen
+  connectWLAN();
+
+  // fetches ssid and pass from eeprom and tries to connect
+  // if it does not connect it starts an access point with the specified name
+  // here  "WordClock"
+  // and goes into a blocking loop awaiting configuration
+  wifiManager.autoConnect("wordclock");
+  // or use this for auto generated name ESP + ChipID
+  //wifiManager.autoConnect();
+
+  // if you get here you have connected to the WiFi
+  Serial.println("Connected.");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.print("Version: ");
+  Serial.println(version);
+  setupDisplay();
+
+  chase(Orange); // run basic screen test and show success
+
+  setupWifi();
+  setupTime();
+
+  //showWifiSuccess();
+}
+
+/**
+ * Displays current time if minute changed
+ */
+void loop() {
+  if (timeStatus() != timeNotSet) {
+    if (lastMinuteWordClock != wordClockMinute) { //update the display only if time has changed
+      getLocalTime();
+      serialTime();
+      displayTime();
+      lastMinuteWordClock = wordClockMinute;
+    } else {
+      displayWifiStatus();
+      getLocalTime();
+    }
+  }
+}
