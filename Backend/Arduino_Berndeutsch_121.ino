d4a28e4a8128d5b2c2e4f823c08f9e6f8ccef8ef
U109281 <U109281@mobi.ch>
2024-01-17 16:12:27 +0100

    initial WebServer added

--- Backend/Arduino_Berndeutsch_121.ino
+++ Backend/Arduino_Berndeutsch_121.ino
@@ -9,6 +9,9 @@
 // Kurt Meister, 2018-12-24 | Edit: 2023-04-29 
 // Thanks to Manuel Meister for refactoring and adding automated summertime conversion.
 //
+// Gérard Tyedmers, 2024-01-15 
+// Web-Interface added
+//
 /////////////////////////////////////////////
 
 
@@ -21,6 +24,27 @@
 #include <Adafruit_NeoPixel.h>  // v1.10.4
 
 char version[] = "V3";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output5State = "off";
+String output4State = "off";
+
+// Assign output variables to GPIO pins
+const int output5 = 5;
+const int output4 = 4;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 int wordClockMinute = 0;
 int wordClockHour = 0;
@@ -52,13 +76,15 @@ unsigned int localPort = 8888;  // local port to listen for UDP packets
 
 static uint32_t Black = Adafruit_NeoPixel::Color(0, 0, 0);
 static uint32_t White = Adafruit_NeoPixel::Color(49, 52, 34);
-static uint32_t Green = Adafruit_NeoPixel::Color(10, 90, 0);
+static uint32_t Grey = Adafruit_NeoPixel::Color(10, 10, 8);
 static uint32_t Red = Adafruit_NeoPixel::Color(90, 0, 0);
-static uint32_t Orange = Adafruit_NeoPixel::Color(70, 30, 0);
+static uint32_t Orange = Adafruit_NeoPixel::Color(75, 30, 0);
 static uint32_t Yellow = Adafruit_NeoPixel::Color(50, 50, 0);
+static uint32_t Green = Adafruit_NeoPixel::Color(10, 90, 0);
 static uint32_t Blue = Adafruit_NeoPixel::Color(0, 20, 85);
 static uint32_t DarkBlue = Adafruit_NeoPixel::Color(0, 10, 40);
-static uint32_t DarkestBlue = Adafruit_NeoPixel::Color(0, 8, 20);
+static uint32_t DarkestBlue = Adafruit_NeoPixel::Color(2, 0, 15);
+static uint32_t Purple = Adafruit_NeoPixel::Color(45, 0, 50);
 
 /*
 | Ä | S | U | I | S | C | H | J | F | Ü | F |
@@ -268,6 +294,13 @@ void displayWifiStatus() {
 void displayTime() {
   blank();
 
+  // Display darker color between 22:00 and 07:00
+  if (wordClockHour > 6 && wordClockHour < 22) {
+    //foregroundColor = White;
+  } else {
+    foregroundColor = Grey;
+  }
+
   // light up "it's" it stays on
   lightup(WordEs, foregroundColor);
   lightup(WordIst, foregroundColor);
@@ -434,6 +467,9 @@ void setupWifi() {
 
   // if you get here you have connected to the WiFi
   Serial.println("Connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
 void setup() {
@@ -455,6 +491,104 @@ void setup() {
  * Displays current time if minute changed
  */
 void loop() {
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    currentTime = millis();
+    previousTime = currentTime;
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              foregroundColor = Orange;
+              output5State = "on";
+              lastMinuteWordClock = 61;
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              foregroundColor = White;
+              output5State = "off";
+              lastMinuteWordClock = 61;
+            } 
+            if (header.indexOf("GET /4/on") >= 0) {
+              foregroundColor = Purple;
+              output4State = "on";
+              lastMinuteWordClock = 61;
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              foregroundColor = White;
+              output4State = "off";
+              lastMinuteWordClock = 61;
+            } 
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>grrd's WordClock</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.println("<p>Color Orange - State " + output5State + "</p>");
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 4  
+            client.println("<p>Color Purple - State " + output4State + "</p>");
+            // If the output4State is off, it displays the ON button       
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+
   if (timeStatus() != timeNotSet) {
     if (lastMinuteWordClock != wordClockMinute) { //update the display only if time has changed
       getLocalTime();
