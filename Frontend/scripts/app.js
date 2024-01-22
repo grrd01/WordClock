e1ad3b984ffb98df5ae803dbc2d5f84837efbade
Gerard Tyedmers <gtyedmers@gmx.net>
2024-01-22 18:34:02 +0100

    bugfix: ios

--- Frontend/scripts/app.js
+++ Frontend/scripts/app.js
@@ -1,35 +1,139 @@
 {
-    let a, l, d, o = 1, c = 0, n = 255, i = 0, r = 0;
-    const g = document.getElementById("clock"), m = document.getElementById("pageClock"),
-        u = document.getElementById("pageSettings"), L = document.getElementById("color");
-
-    function e(e) {
-        document.styleSheets[0].cssRules[3].style.fill = e, document.styleSheets[0].cssRules[3].style.textShadow = "0 0 10px " + e, document.getElementsByTagName("a")[0].style.color = e, document.getElementsByTagName("a")[1].style.color = e
+    let date;
+    let hour;
+    let minute;
+    let darkMode = 1;
+    let rainbow = 0;
+    let rainbowRed = 255;
+    let rainbowGreen = 0;
+    let rainbowBlue = 0;
+    const clock = document.getElementById("clock");
+    const pageClock = document.getElementById("pageClock");
+    const pageSettings = document.getElementById("pageSettings");
+    const color = document.getElementById("color");
+    function setTime () {
+        date = new Date();
+        hour = date.getHours() % 12;
+        if (rainbow) {
+            if (rainbowRed && !rainbowBlue) {
+                rainbowRed -= 1;
+                rainbowGreen += 1;
+            } else if (rainbowGreen) {
+                rainbowGreen -= 1;
+                rainbowBlue += 1;
+            } else {
+                rainbowBlue -= 1;
+                rainbowRed += 1;
+            }
+            fChangeColor("rgb(" + rainbowRed + ", " + rainbowGreen + ", " + rainbowBlue + ")");
+        }
+        if (darkMode && (date.getHours() >=22 || date.getHours() < 7)) {
+            document.getElementsByTagName("body")[0].classList.add("dark");
+        } else {
+            document.getElementsByTagName("body")[0].classList.remove("dark");
+        }
+        if (minute === date.getMinutes()) {
+            return;
+        }
+        minute = date.getMinutes();
+        clock.classList.remove(...clock.classList);
+        if (minute >= 55) {
+            clock.classList.add("M5", "MV");
+        } else if (minute >= 50) {
+            clock.classList.add("M10", "MV");
+        } else if (minute >= 45) {
+            clock.classList.add("M15", "MV");
+        } else if (minute >= 40) {
+            clock.classList.add("M20", "MV");
+        } else if (minute >= 35) {
+            clock.classList.add("M5", "MA", "M30");
+        } else if (minute >= 30) {
+            clock.classList.add("M30");
+        } else if (minute >= 25) {
+            clock.classList.add("M5", "MV", "M30");
+        } else if (minute >= 20) {
+            clock.classList.add("M20", "MA");
+        } else if (minute >= 15) {
+            clock.classList.add("M15", "MA");
+        } else if (minute >= 10) {
+            clock.classList.add("M10", "MA");
+        } else if (minute >= 5) {
+            clock.classList.add("M5", "MA");
+        }
+        if (minute >= 25) {
+            hour += 1;
+        }
+        clock.classList.add("H" + hour.toString());
+        clock.classList.add("M" + (minute % 5).toString());
     }
+    setInterval(setTime, 100);
 
-    function t(t) {
-        t !== c && (document.getElementById("rainbowMode").children[0].classList.toggle("hide"), document.getElementById("rainbowMode").children[1].classList.toggle("hide")), c = t, c || e(L.value)
+    function fShowSettings() {
+        // Fix for Firefox OnKeydown
+        document.activeElement.blur();
+        pageClock.classList.remove("swipe-out-right");
+        pageSettings.classList.remove("swipe-in-left");
+        pageClock.classList.add("swipe-out");
+        pageSettings.classList.add("swipe-in");
     }
-
-    function s(e) {
-        e !== o && (document.getElementById("darkMode").children[0].classList.toggle("hide"), document.getElementById("darkMode").children[1].classList.toggle("hide")), o = e
+    function fChangeColor(color_in) {
+        document.styleSheets[0].cssRules[3].style.fill = color_in;
+        document.styleSheets[0].cssRules[3].style.textShadow = "0 0 10px " + color_in;
+        document.getElementsByTagName("a")[0].style.color = color_in;
+        document.getElementsByTagName("a")[1].style.color = color_in;
     }
+    function fRainbow(rain_in) {
+        if (rain_in !== rainbow) {
+            document.getElementById("rainbowMode").children[0].classList.toggle("hide");
+            document.getElementById("rainbowMode").children[1].classList.toggle("hide");
+        }
+        rainbow = rain_in;
+        if (!rainbow) {
+            fChangeColor(color.value);
+        }
+    }
+    function fDarkMode(dark_in) {
+        if (dark_in !== darkMode) {
+            document.getElementById("darkMode").children[0].classList.toggle("hide");
+            document.getElementById("darkMode").children[1].classList.toggle("hide");
+        }
+        darkMode = dark_in;
+    }
+    function fHideSettings() {
+        let red = parseInt(color.value.substring(1,3), 16);
+        let green = parseInt(color.value.substring(3,5), 16);
+        let blue = parseInt(color.value.substring(5,7), 16);
+        pageClock.classList.remove("swipe-out");
+        pageSettings.classList.remove("swipe-in");
+        pageClock.classList.add("swipe-out-right");
+        pageSettings.classList.add("swipe-in-left");
+        localStorage.setItem("wc_color", color.value);
+        localStorage.setItem("wc_rainbow", rainbow);
+        localStorage.setItem("wc_darkmode", darkMode);
+        setTimeout(function() {
+            window.location.search = ("&red=" + red + "&green=" + green + "&blue=" + blue + "&rainbow=" + rainbow + "&darkmode=" + darkMode);
+        }, 500);
 
-    setInterval((function () {
-        a = new Date, l = a.getHours() % 12, c && (n && !r ? (n -= 1, i += 1) : i ? (i -= 1, r += 1) : (r -= 1, n += 1), e("rgb(" + n + ", " + i + ", " + r + ")")), o && (a.getHours() >= 22 || a.getHours() < 7) ? document.getElementsByTagName("body")[0].classList.add("dark") : document.getElementsByTagName("body")[0].classList.remove("dark"), d !== a.getMinutes() && (d = a.getMinutes(), g.classList.remove(...g.classList), d >= 55 ? g.classList.add("M5", "MV") : d >= 50 ? g.classList.add("M10", "MV") : d >= 45 ? g.classList.add("M15", "MV") : d >= 40 ? g.classList.add("M20", "MV") : d >= 35 ? g.classList.add("M5", "MA", "M30") : d >= 30 ? g.classList.add("M30") : d >= 25 ? g.classList.add("M5", "MV", "M30") : d >= 20 ? g.classList.add("M20", "MA") : d >= 15 ? g.classList.add("M15", "MA") : d >= 10 ? g.classList.add("M10", "MA") : d >= 5 && g.classList.add("M5", "MA"), d >= 25 && (l += 1), g.classList.add("H" + l.toString()), g.classList.add("M" + (d % 5).toString()))
-    }), 100), document.getElementById("settings").addEventListener("click", (function () {
-        document.activeElement.blur(), m.classList.remove("swipe-out-right"), u.classList.remove("swipe-in-left"), m.classList.add("swipe-out"), u.classList.add("swipe-in")
-    })), document.getElementById("settingsClose").addEventListener("click", (function () {
-        let e = parseInt(L.value.substring(1, 3), 16), t = parseInt(L.value.substring(3, 5), 16),
-            s = parseInt(L.value.substring(5, 7), 16);
-        m.classList.remove("swipe-out"), u.classList.remove("swipe-in"), m.classList.add("swipe-out-right"), u.classList.add("swipe-in-left"), localStorage.setItem("wc_color", L.value), localStorage.setItem("wc_rainbow", c), localStorage.setItem("wc_darkmode", o), setTimeout((function () {
-            window.location.search = "&red=" + e + "&green=" + t + "&blue=" + s + "&rainbow=" + c + "&darkmode=" + o
-        }), 500)
-    })), document.getElementById("color").addEventListener("change", t => {
-        e(L.value)
-    }, !1), document.getElementById("rainbowMode").addEventListener("click", e => {
-        t(1 - c)
-    }), document.getElementById("darkMode").addEventListener("click", e => {
-        s(1 - o)
-    }), localStorage.getItem("wc_color") && (L.value = localStorage.getItem("wc_color"), e(L.value)), localStorage.getItem("wc_rainbow") && t(parseInt(localStorage.getItem("wc_rainbow"))), localStorage.getItem("wc_darkmode") && s(parseInt(localStorage.getItem("wc_darkmode")))
+    }
+    document.getElementById("settings").addEventListener("click", fShowSettings);
+    document.getElementById("settingsClose").addEventListener("click", fHideSettings);
+    document.getElementById("color").addEventListener("change", (ignore) => {
+        fChangeColor(color.value);
+    }, false);
+    document.getElementById("rainbowMode").addEventListener("click", (ignore) => {
+        fRainbow(1 - rainbow);
+    });
+    document.getElementById("darkMode").addEventListener("click", (ignore) => {
+        fDarkMode(1 - darkMode);
+    });
+    if (localStorage.getItem("wc_color")) {
+        color.value = localStorage.getItem("wc_color");
+        fChangeColor(color.value);
+    }
+    if (localStorage.getItem("wc_rainbow")) {
+        fRainbow(parseInt(localStorage.getItem("wc_rainbow")));
+    }
+    if (localStorage.getItem("wc_darkmode")) {
+        fDarkMode(parseInt(localStorage.getItem("wc_darkmode")));
+    }
 }
