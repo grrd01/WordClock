3527e8a81725ee6240c89701a9631ce52e0ef523
Gerard Tyedmers <gtyedmers@gmx.net>
2026-09-19 14:31:10 +0200

    manual completed

--- Manual/scripts/manual.js
+++ Manual/scripts/manual.js
@@ -1,17 +1,10 @@
 (function () {
 	"use strict";
 
-	const body = document.body;
-	const clock = document.querySelector("#pageWordClock .t");
 	const pages = Array.from(document.querySelectorAll(".p"));
 	const prevButton = document.querySelector("#prev");
 	const nextButton = document.querySelector("#next");
 	const pageIndicator = document.querySelector("#pageIndicator");
-	const dynamicClockClasses = [
-		"M0", "M1", "M2", "M3", "M4",
-		"M5", "M10", "M15", "M20", "M30", "MA", "MV",
-		"H0", "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11"
-	];
 	const pageNames = pages.map(function (page) {
 		return page.id.replace(/^page/i, "").toLowerCase();
 	});
@@ -19,9 +12,6 @@
 	let minute = -1;
 	let currentPageIndex = 0;
 
-	if (!clock) {
-		return;
-	}
 
 	if (pages.length === 0) {
 		return;
@@ -76,6 +66,16 @@
 			}
 		});
 
+		if (prevButton) {
+			prevButton.hidden = currentPageIndex === 0;
+			prevButton.setAttribute("aria-hidden", prevButton.hidden.toString());
+		}
+
+		if (nextButton) {
+			nextButton.hidden = currentPageIndex === pages.length - 1;
+			nextButton.setAttribute("aria-hidden", nextButton.hidden.toString());
+		}
+
 		updatePageIndicator();
 		updatePageUrl();
 	}
@@ -94,57 +94,4 @@
 		});
 	}
 
-	function setTime() {
-		const date = new Date();
-
-		if (date.getHours() >= 22 || date.getHours() < 7) {
-			body.classList.add("d");
-		} else {
-			body.classList.remove("d");
-		}
-
-		if (minute === date.getMinutes()) {
-			return;
-		}
-
-		minute = date.getMinutes();
-		clock.classList.remove(...dynamicClockClasses);
-
-		if (minute >= 55) {
-			clock.classList.add("M5", "MV");
-		} else if (minute >= 50) {
-			clock.classList.add("M10", "MV");
-		} else if (minute >= 45) {
-			clock.classList.add("M15", "MV");
-		} else if (minute >= 40) {
-			clock.classList.add("M20", "MV");
-		} else if (minute >= 35) {
-			clock.classList.add("M5", "MA", "M30");
-		} else if (minute >= 30) {
-			clock.classList.add("M30");
-		} else if (minute >= 25) {
-			clock.classList.add("M5", "MV", "M30");
-		} else if (minute >= 20) {
-			clock.classList.add("M20", "MA");
-		} else if (minute >= 15) {
-			clock.classList.add("M15", "MA");
-		} else if (minute >= 10) {
-			clock.classList.add("M10", "MA");
-		} else if (minute >= 5) {
-			clock.classList.add("M5", "MA");
-		}
-
-		let hour = date.getHours();
-		if (minute >= 25) {
-			hour += 1;
-		}
-		hour %= 12;
-
-		clock.classList.add("H" + hour.toString());
-		clock.classList.add("M" + (minute % 5).toString());
-	}
-
-	setTime();
-	setInterval(setTime, 100);
 }());
-
